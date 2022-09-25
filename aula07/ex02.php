e7c36a156eb33b950fb080c06070d1615f9c8470
António Silva <100034891+antoniosilva23@users.noreply.github.com>
2022-09-25 19:57:46 +0100

    operadores relacionais, igual ou identico

---
+++ aula07/ex02.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Relacionais</title>
+</head>
+<body>
+    <div>
+
+        <?php 
+            $a = 3;
+            $b = "3";
+            $r = ($a == $b)?"SIM":"NAO";
+            echo "As variaveis A e B são iguais? $r";
+        ?>
+
+    </div>
+</body>
+</html>
