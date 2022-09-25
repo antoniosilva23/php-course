9e276801372589864b0d46c55f39d9b4aa1cc920
António Silva <100034891+antoniosilva23@users.noreply.github.com>
2022-09-25 11:28:44 +0100

    php variables

---
+++ aula04/tipos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First PHP</title>
+</head>
+<body>
+    <h1>Estou aprendendo PHP!</h1>
+    <h2>Variaveis em PHP</h2>
+
+    <?php 
+
+        $nome = "Maria";
+        $idade = 24;
+        echo "$nome tem $idade anos!";
+        
+    ?>
+
+</body>
+</html>
